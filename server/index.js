406a4e886a25c74cb17dae4be45aea8c32925748
davidhardin <thisgoestodavid@gmail.com>
2023-12-27 14:34:35 -0700

    building typedefs

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
-import express from 'express'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js'
-import schema from './schema/schema.js'
+import typeDefs from './schema/typeDefs.js'
+import resolvers from './resolvers/resolvers.js'
 
 const port = process.env.PORT || 5001
 
 connectDB()
 
-const app = express()
+const server = new ApolloServer({ typeDefs, resolvers })
 
-app.listen(port, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
-)
+startStandaloneServer(server, {
+  listen: { port: process.env.PORT || 5001 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
