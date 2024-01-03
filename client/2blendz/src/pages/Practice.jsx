48d8dda84847a95cd1dee4b765428c1545bb4cb7
davidhardin <thisgoestodavid@gmail.com>
2024-01-03 15:33:38 -0700

    blendz

--- client/2blendz/src/pages/Practice.jsx
+++ client/2blendz/src/pages/Practice.jsx
@@ -1,5 +1,11 @@
+import blendz from '../assets/images/88blendz.jpeg'
+
 const Practice = () => {
-  return <div className=''>Practice</div>
+  return (
+    <div className='bg-black'>
+      <img alt='' src={blendz} className='invert'></img>
+    </div>
+  )
 }
 
 export default Practice
