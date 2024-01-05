7077eb50afbf792f387afeff76a980f0e41a7817
davidhardin <thisgoestodavid@gmail.com>
2024-01-04 21:16:32 -0700

    favicon stuff

--- client/2blendz/src/pages/Practice.jsx
+++ client/2blendz/src/pages/Practice.jsx
@@ -1,9 +1,16 @@
-import blendz from '../assets/images/88blendz.jpeg'
+import blendz from '../assets/images/naroBlends.jpeg'
 
 const Practice = () => {
   return (
-    <div className='bg-black'>
-      <img alt='' src={blendz} className='invert'></img>
+    <div className='bg-black columns-2 h-screen'>
+      <div className='bg-indigo-500 h-full'>
+        <div className=''>
+          <img alt='' src={blendz} className='invert ml-5'></img>
+          <p className='text-white'>Book</p>
+        </div>
+      </div>
+
+      <div className='text-white h-full'>Text TEXT tExT</div>
     </div>
   )
 }
