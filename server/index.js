0bdc861952913c1de38a79173b3daf1b7f494339
davidhardin <thisgoestodavid@gmail.com>
2024-05-14 04:33:28 -0600

    whateveR

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.use(
   })
 )
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+// app.use(express.static(path.join(__dirname, 'client/build')))
 // app.use(express.static('client/public'))
 
 app.get('*', (req, res) => res.send('hello'))
