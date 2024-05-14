fedbc1b3de4eaf5612e6cf3e24484c2d9cd669f1
davidhardin <thisgoestodavid@gmail.com>
2024-05-14 02:45:54 -0600

    yodel

--- server/index.js
+++ server/index.js
@@ -46,13 +46,13 @@ app.use(
   })
 )
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 // app.use(express.static('client-app/build'));
 
 //
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+// })
 
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 5002 }, resolve))
