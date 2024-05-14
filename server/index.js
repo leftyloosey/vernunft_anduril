d2599534ffb5f0e11006e83e8bd3c93a86ed6af6
davidhardin <thisgoestodavid@gmail.com>
2024-05-14 03:00:10 -0600

    last one

--- server/index.js
+++ server/index.js
@@ -47,12 +47,12 @@ app.use(
 )
 
 // app.use(express.static('public'))
-// app.use(express.static('client-app/build'));
+app.use(express.static('client/public'))
 
 //
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: port }, resolve))
