1d1f6359b7359e81fc6ef463a06139aef96e6df7
davidhardin <thisgoestodavid@gmail.com>
2024-05-14 04:27:36 -0600

    whateveR

--- server/index.js
+++ server/index.js
@@ -54,12 +54,11 @@ app.use(
 app.use(express.static(path.join(__dirname, 'client/build')))
 // app.use(express.static('client/public'))
 
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-)
-app.get('/', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-)
+app.get('*', (req, res) => res.send('hello'))
+// res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+
+app.get('/', (req, res) => res.send('hello'))
+// res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 
 // app.use(express.static('public'))
 
