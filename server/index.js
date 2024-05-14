4b4a9e7c21efe6ffd493a1ac76022fb13bedacd7
davidhardin <thisgoestodavid@gmail.com>
2024-05-14 11:18:23 -0600

    next day woes

--- server/index.js
+++ server/index.js
@@ -52,25 +52,26 @@ app.use(
 )
 
 // app.use(express.static(path.join(__dirname, 'client/build')))
-// app.use(express.static('client/public'))
+app.use(express.static('client/public'))
 
-app.get('*', (req, res) => {
-  console.log(res)
+// app.get('*', (req, res) => {
+//   console.log(res)
 
-  res.send('hello')
-})
+//   res.send('hello')
+// })
 // res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 
-app.get('/', (req, res) => res.send('hello'))
-console.log('hello')
+app.get('/hello', (req, res) => {
+  res.send('hello')
+  console.log(res)
+})
 // res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 
 // app.use(express.static('public'))
 
-//
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-// })
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/public', 'index.html'))
+})
 
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: port }, resolve))
