630cd82a1e8c3622257d0dc2eb4a3db30fff16ec
davidhardin <thisgoestodavid@gmail.com>
2024-05-14 12:10:36 -0600

    oh no woes

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ await server.start()
 // and our expressMiddleware function.
 app.use(express.static(path.join(__dirname, '../client/build')))
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 })
 
 app.use(
